3e1fb428842d28313dc5eeab1e1abcb0f8e9bf36
flawlessBadger <jozefferko9@gmail.com>
2017-11-30 14:22:27 +0100

    another one

--- IntellijCode/src/Main.java
+++ IntellijCode/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
- public static void main(String[] args){
-     System.out.print("what did i do");
- }
+    public static void main(String[] args) {
+        System.out.print("what did i do");
+    }
 }
